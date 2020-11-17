5ba5efe2b521c429c3bd3de540f6aca995d690a7
amtesire <tesiregisele@gmail.com>
2020-11-17 07:01:37 +0200

    added new credential

--- credential.py
+++ credential.py
@@ -14,3 +14,36 @@ class Credential:
 		self.credential_account = credential_account = credential_account
 		self.account_name = account_name
 		self.password = password
+
+    @classmethod
+	def check_user(cls,first_name,password):
+		
+		current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
+
+
+	def save_credentials(self):
+		'''
+		Function to save a newly created user credentials
+		'''
+
+		Credential.list_of_credentials.append(self)
+
+
+	@classmethod
+	def delete_credentials(self):
+		'''
+		Function to delete  credentials
+		'''
+
+		Credential.list_of_credentials.remove(self)
+
+	def generate_password(size=8, char=string.ascii_lowercase+string.ascii_lowercase+string.digits):
+		'''
+		Function to generate a secure 8 character password for a user.
+		'''
+		password_gen=''.join(random.choice(char) for _ in range(size))
+		return password_gen
